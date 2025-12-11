f5f9c0d97dc7a5d4bd1a1ebe8455c9098eccf8fa
birdnero <neroforwork@gmail.com>
2025-12-11 17:02:13 +0200

    fixed all previous problems | add save btn (not completed)

--- frontend/src/components/PlayLink.tsx
+++ frontend/src/components/PlayLink.tsx
@@ -1,14 +1,19 @@
 import { Button } from "antd"
 import type { Play } from "../utils/ApiDtos"
+import React from "react";
 
-const PlayLink: React.FC<{
-  play: Play
-}> = ({ play }) => {
-
-
-  return (<Button href={"/plays/" + play.play_id.toString()} variant="filled" shape="round" color="pink" size="middle">
-    {play.name}
-  </Button>)
-}
+const PlayLink = React.memo(({ play }: { play: Play }) => {
+  return (
+    <Button
+      href={"/plays/" + play.play_id}
+      variant="filled"
+      shape="round"
+      color="pink"
+      size="middle"
+    >
+      {play.name}
+    </Button>
+  );
+});
 
 export default PlayLink
